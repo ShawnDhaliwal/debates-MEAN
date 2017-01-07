61f13746e41b15f01323458b0a27e3f07258ef1b
Shawn Dhaliwal <Dhaliwal@Shawns-MacBook-Pro.local>
2017-01-06 23:35:06 -0800

    Fixed style issues on mobile and small screen sizes

--- app/controllers/custom-controller.js
+++ app/controllers/custom-controller.js
@@ -6,10 +6,11 @@ app.controller('signup-controller', function($scope) {
 app.controller('login-controller', function($scope) {
 });
 
+
 app.controller('topics-controller', function($scope) {
 	$scope.topics = [
-	  {title: 'Header 1', score: 5, category: 'Politics'},
-	  {title: 'Header 2', score: 2, category: 'Sports'},
+	  {title: 'Header 1Header 1Header 1Header 1Header 1Header 1Header 1Header 1Header 1Header 1Header 1Header 1', agree: 3, disagree: 3, score: 0, category: 'Politics'},
+	  {title: 'HeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeaderHeader', score: 2, category: 'Sports'},
 	  {title: 'Header 3', score: 15, category: 'News'},
 	  {title: 'Header 4', score: 9, category: 'Science'},
 	  {title: 'Header 5', score: 4, category: 'Equality'}
@@ -17,5 +18,13 @@ app.controller('topics-controller', function($scope) {
 	$scope.addTopic = function(){
 	  $scope.topics.push({title: $scope.topic.title, score: 0, category: $scope.topic.category});
 	  swal("Topic Created","","success"); 
+	};
+	$scope.incrementAgree = function(topic) {
+  		topic.score += 1;
+  		topic.agree += 1;
+	};
+	$scope.incrementDisagree = function(topic) {
+  		topic.score -= 1;
+  		topic.disagree += 1;
 	};
 });
